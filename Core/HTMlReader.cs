5e1f3392e7cbe2571f849aac643627ed40733682
MuryloSaladino <murylosaladino@gmail.com>
2024-08-21 15:48:51 -0300

    refact: regex and main substring

--- Core/HTMlReader.cs
+++ Core/HTMlReader.cs
@@ -2,12 +2,15 @@ using System.Text.RegularExpressions;
 
 public static class HTMLReader
 {
-    private static readonly Regex AnchorRegex = new(@"<a\s+[^>]*href\s*=\s*['""][^'""]*['""][^>]*>(.*?)</a>");
+    private static readonly Regex AnchorRegex = new(@"<a\s+[^>]*href\s*=\s*['""][^'""]*['""][^>]*>");
 
-    public static string[] GetLinks(string html)
+    public static string[] GetWikipediaLinks(string html)
     {
+        int start = html.IndexOf("<main");
+        int end = html.IndexOf("</main>") - start;
+
         return AnchorRegex
-            .Matches(html)
+            .Matches(html.Substring(start, end))
             .Select(x => x.Value)
             .ToArray();
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 var page = await Browser.GetPage("https://pt.wikipedia.org/wiki/Audi");
-var links = HTMLReader.GetLinks(page);
+var links = HTMLReader.GetWikipediaLinks(page);
 
 foreach(var link in links)
     Console.WriteLine(link);
