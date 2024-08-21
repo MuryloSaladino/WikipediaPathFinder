714ce9418bb0cf438823c82af8fb36c920b25754
MuryloSaladino <murylosaladino@gmail.com>
2024-08-21 15:02:40 -0300

    feat: browser

--- Core/WikipediaBrowser.cs
+++ Core/WikipediaBrowser.cs
@@ -2,7 +2,20 @@ using System.Net;
 
 public static class Browser
 {
+    private static readonly HttpClient Client = new(
+        new HttpClientHandler {
+            Proxy = new WebProxy {
+                Address = new Uri("http://rb-proxy-ca1.bosch.com:8080"),
+                Credentials = new NetworkCredential("disrct", "etsps2024401")
+            }
+        }
+    );
 
+    private static async Task<string> GetPage(string link)
+    {
+        var response = await Client.GetAsync(link);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
 
 public class LinkNotFoundException : Exception {}
