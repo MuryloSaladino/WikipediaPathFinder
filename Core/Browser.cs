372debdfd01c370d0a489a66d45b3d4f97d8f621
MuryloSaladino <murylosaladino@gmail.com>
2024-11-25 08:30:50 -0300

    fix: leakage

--- Core/Browser.cs
+++ Core/Browser.cs
@@ -2,14 +2,7 @@ using System.Net;
 
 public static class Browser
 {
-    private static readonly HttpClient Client = new(
-        new HttpClientHandler {
-            Proxy = new WebProxy {
-                Address = new Uri("http://rb-proxy-ca1.bosch.com:8080"),
-                Credentials = new NetworkCredential("disrct", "etsps2024401")
-            }
-        }
-    );
+    private static readonly HttpClient Client = new();
 
     public static async Task<string> GetPage(string link)
     {
